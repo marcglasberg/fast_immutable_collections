e8c1e267fdf43c21e80e624752503c8cbcf22bab
Philippe Fanaro <philippefanaro@gmail.com>
2020-09-11 10:08:08 -0300

    changed the location of the emitter and list base benchmark to the utils folder

--- benchmark/cases/remove.dart
+++ benchmark/cases/remove.dart
@@ -6,8 +6,8 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart'
     show IList;
 import 'package:kt_dart/collection.dart' show KtList;
 
-import '../list_benchmark_base.dart' show ListBenchmarkBase;
-import '../table_score_emitter.dart' show TableScoreEmitter;
+import '../utils/list_benchmark_base.dart' show ListBenchmarkBase;
+import '../utils/table_score_emitter.dart' show TableScoreEmitter;
 
 class RemoveBenchmark {
   static void report() {
