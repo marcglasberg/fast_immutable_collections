37a587f479fffda400aec1df71aaa6765524711b
Marcelo Glasberg <marcglasberg@gmail.com>
2020-09-12 16:47:04 -0300

    Refactor.

--- benchmark/cases/add.dart
+++ benchmark/cases/add.dart
@@ -2,6 +2,7 @@ import 'package:benchmark_harness/benchmark_harness.dart' show ScoreEmitter;
 import 'package:built_collection/built_collection.dart' show BuiltList, ListBuilder;
 import 'package:kt_dart/kt.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+import 'package:meta/meta.dart';
 
 import '../utils/list_benchmark_base.dart' show ListBenchmarkBase;
 import '../utils/table_score_emitter.dart' show TableScoreEmitter;
@@ -12,11 +13,11 @@ class AddBenchmark {
   static void report() {
     final TableScoreEmitter tableScoreEmitter = TableScoreEmitter(reportName: 'list_add');
 
-    _ListAddBenchmark(emitter: tableScoreEmitter).report();
-    _IListAddBenchmark(emitter: tableScoreEmitter).report();
-    _KtListAddBenchmark(emitter: tableScoreEmitter).report();
-    _BuiltListAddBenchmark1(emitter: tableScoreEmitter).report();
-    _BuiltListAddBenchmark2(emitter: tableScoreEmitter).report();
+    _ListAddBenchmark(runs: 10000, size: 1000, emitter: tableScoreEmitter).report();
+    _IListAddBenchmark(runs: 10000, size: 1000, emitter: tableScoreEmitter).report();
+    _KtListAddBenchmark(runs: 10000, size: 1000, emitter: tableScoreEmitter).report();
+    _BuiltListAddBenchmark1(runs: 10000, size: 1000, emitter: tableScoreEmitter).report();
+    _BuiltListAddBenchmark2(runs: 10000, size: 1000, emitter: tableScoreEmitter).report();
 
     tableScoreEmitter.saveReport();
   }
@@ -25,85 +26,122 @@ class AddBenchmark {
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 class _ListAddBenchmark extends ListBenchmarkBase {
-  _ListAddBenchmark({ScoreEmitter emitter}) : super('List (Mutable)', emitter: emitter);
+  _ListAddBenchmark({
+    @required int runs,
+    @required int size,
+    @required ScoreEmitter emitter,
+  }) : super('List (Mutable) $size', runs: runs, size: size, emitter: emitter);
 
-  List<int> _list;
+  List<int> list;
 
   @override
-  void setup() => _list = <int>[];
+  void setup() {
+    list = <int>[];
+    for (int i = 0; i < size; i++) list.add(i);
+  }
 
   @override
   void run() {
-    for (int i = 0; i < 100; i++) _list.add(i);
+    list.add(123);
   }
 }
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 class _IListAddBenchmark extends ListBenchmarkBase {
-  _IListAddBenchmark({ScoreEmitter emitter}) : super('IList', emitter: emitter);
+  _IListAddBenchmark({
+    @required int runs,
+    @required int size,
+    @required ScoreEmitter emitter,
+  }) : super('IList $size', runs: runs, size: size, emitter: emitter);
 
-  IList<int> _iList;
+  IList<int> iList;
 
   @override
-  void setup() => _iList = IList<int>();
+  void setup() {
+    iList = IList<int>();
+    for (int i = 0; i < size; i++) iList = iList.add(i);
+  }
 
   @override
   void run() {
-    for (int i = 0; i < 100; i++) _iList = _iList.add(i);
+    iList.add(123);
   }
 }
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 class _KtListAddBenchmark extends ListBenchmarkBase {
-  _KtListAddBenchmark({ScoreEmitter emitter}) : super('KtList', emitter: emitter);
+  _KtListAddBenchmark({
+    @required int runs,
+    @required int size,
+    @required ScoreEmitter emitter,
+  }) : super('KtList $size', runs: runs, size: size, emitter: emitter);
 
   KtList<int> _ktList;
 
   @override
-  void setup() => _ktList = [].toImmutableList();
+  void setup() {
+    var list = <int>[];
+    for (int i = 0; i < size; i++) list.add(i);
+    _ktList = list.toImmutableList();
+  }
 
   @override
   void run() {
-    for (int i = 0; i < 100; i++) _ktList = _ktList.plusElement(1);
+    _ktList = _ktList.plusElement(1);
   }
 }
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 class _BuiltListAddBenchmark1 extends ListBenchmarkBase {
-  _BuiltListAddBenchmark1({ScoreEmitter emitter}) : super('BuiltList', emitter: emitter);
+  _BuiltListAddBenchmark1({
+    @required int runs,
+    @required int size,
+    @required ScoreEmitter emitter,
+  }) : super('BuiltList1 $size', runs: runs, size: size, emitter: emitter);
 
   BuiltList<int> _builtList;
 
   @override
-  void setup() => _builtList = BuiltList<int>();
+  void setup() {
+    var list = <int>[];
+    for (int i = 0; i < size; i++) list.add(i);
+    _builtList = BuiltList<int>(list);
+  }
 
   @override
   void run() {
-    for (int i = 0; i < 100; i++)
-      _builtList = _builtList.rebuild((ListBuilder<int> listBuilder) => listBuilder.add(i));
+    _builtList.rebuild((ListBuilder<int> listBuilder) => listBuilder.add(123));
   }
 }
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 class _BuiltListAddBenchmark2 extends ListBenchmarkBase {
-  _BuiltListAddBenchmark2({ScoreEmitter emitter}) : super('BuiltList', emitter: emitter);
+  static const innerRuns = 1000;
+
+  _BuiltListAddBenchmark2({
+    @required int runs,
+    @required int size,
+    @required ScoreEmitter emitter,
+  }) : super('BuiltList2 $size', runs: runs ~/ innerRuns, size: size, emitter: emitter);
 
   BuiltList<int> _builtList;
 
   @override
-  void setup() => _builtList = BuiltList<int>();
+  void setup() {
+    var list = <int>[];
+    for (int i = 0; i < size; i++) list.add(i);
+    _builtList = BuiltList<int>(list);
+  }
 
   @override
   void run() {
-    var listBuilder = _builtList.toBuilder();
-    for (int i = 0; i < 100; i++) {
-      listBuilder.add(i);
-    }
-    _builtList = listBuilder.build();
+    ListBuilder<int> listBuilder = _builtList.toBuilder();
+    for (int i = 0; i < innerRuns; i++) listBuilder.add(123);
+    listBuilder.build();
   }
 }
 
--- benchmark/utils/list_benchmark_base.dart
+++ benchmark/utils/list_benchmark_base.dart
@@ -1,17 +1,25 @@
-import 'package:benchmark_harness/benchmark_harness.dart'
-    show BenchmarkBase, ScoreEmitter;
+import 'package:benchmark_harness/benchmark_harness.dart' show BenchmarkBase, ScoreEmitter;
+import 'package:meta/meta.dart';
 
-List<int> getDummyList({int length = ListBenchmarkBase.totalRuns}) =>
-    List<int>.generate(length, (_) => 1);
+// List<int> getDummyList({int length = ListBenchmarkBase.totalRuns}) =>
+//     List<int>.generate(length, (_) => 1);
 
 class ListBenchmarkBase extends BenchmarkBase {
-  static const int totalRuns = 10000;
+  //
+  final int runs;
+  final int size;
 
-  const ListBenchmarkBase(String name, {ScoreEmitter emitter})
-      : super(name, emitter: emitter);
+  const ListBenchmarkBase(
+    String name, {
+    @required this.runs,
+    @required this.size,
+    ScoreEmitter emitter,
+  })  : assert(runs != null && runs > 0),
+        assert(size != null && size > 0),
+        super(name, emitter: emitter);
 
   @override
   void exercise() {
-    for (int i = 0; i < totalRuns; i++) run();
+    for (int i = 0; i < runs; i++) run();
   }
 }
