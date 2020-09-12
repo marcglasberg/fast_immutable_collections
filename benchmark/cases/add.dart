ac9dbf1a27bc05b9f223d55b8e5e6f328a2c5a8c
Marcelo Glasberg <marcglasberg@gmail.com>
2020-09-12 17:17:31 -0300

    Result.

--- benchmark/cases/add.dart
+++ benchmark/cases/add.dart
@@ -13,24 +13,24 @@ class AddBenchmark {
   static void report() {
     final TableScoreEmitter tableScoreEmitter = TableScoreEmitter(reportName: 'list_add');
 
-    int runs = 10000;
-    int size =  10;
+    int runs = 5000;
+    int size = 10;
     _ListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _IListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _KtListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _BuiltListAddBenchmark1(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _BuiltListAddBenchmark2(runs: runs, size: size, emitter: tableScoreEmitter).report();
 
-    runs = 10000;
-    size =  1000;
+    runs = 5000;
+    size = 1000;
     _ListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _IListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _KtListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _BuiltListAddBenchmark1(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _BuiltListAddBenchmark2(runs: runs, size: size, emitter: tableScoreEmitter).report();
 
-    runs = 10000;
-    size =  100000;
+    runs = 5000;
+    size = 100000;
     _ListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _IListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
     _KtListAddBenchmark(runs: runs, size: size, emitter: tableScoreEmitter).report();
@@ -61,6 +61,8 @@ class _ListAddBenchmark extends ListBenchmarkBase {
   @override
   void run() {
     list.add(123);
+    list.add(345);
+    list.add(567);
   }
 }
 
@@ -74,6 +76,7 @@ class _IListAddBenchmark extends ListBenchmarkBase {
   }) : super('IList $size', runs: runs, size: size, emitter: emitter);
 
   IList<int> iList;
+  IList<int> result;
 
   @override
   void setup() {
@@ -83,7 +86,7 @@ class _IListAddBenchmark extends ListBenchmarkBase {
 
   @override
   void run() {
-    iList.add(123);
+    result = iList.add(123).add(345).add(567);
   }
 }
 
@@ -96,18 +99,19 @@ class _KtListAddBenchmark extends ListBenchmarkBase {
     @required ScoreEmitter emitter,
   }) : super('KtList $size', runs: runs, size: size, emitter: emitter);
 
-  KtList<int> _ktList;
+  KtList<int> ktList;
+  KtList<int> result;
 
   @override
   void setup() {
     var list = <int>[];
     for (int i = 0; i < size; i++) list.add(i);
-    _ktList = list.toImmutableList();
+    ktList = list.toImmutableList();
   }
 
   @override
   void run() {
-    _ktList.plusElement(1);
+    result = ktList.plusElement(123).plusElement(345).plusElement(567);
   }
 }
 
@@ -120,25 +124,29 @@ class _BuiltListAddBenchmark1 extends ListBenchmarkBase {
     @required ScoreEmitter emitter,
   }) : super('BuiltList1 $size', runs: runs, size: size, emitter: emitter);
 
-  BuiltList<int> _builtList;
+  BuiltList<int> builtList;
+  BuiltList<int> result;
 
   @override
   void setup() {
     var list = <int>[];
     for (int i = 0; i < size; i++) list.add(i);
-    _builtList = BuiltList<int>(list);
+    builtList = BuiltList<int>(list);
   }
 
   @override
   void run() {
-    _builtList.rebuild((ListBuilder<int> listBuilder) => listBuilder.add(123));
+    result = builtList
+        .rebuild((ListBuilder<int> listBuilder) => listBuilder.add(123))
+        .rebuild((ListBuilder<int> listBuilder) => listBuilder.add(345))
+        .rebuild((ListBuilder<int> listBuilder) => listBuilder.add(678));
   }
 }
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
 class _BuiltListAddBenchmark2 extends ListBenchmarkBase {
-  static const innerRuns = 100;
+  static const innerRuns = 50;
 
   _BuiltListAddBenchmark2({
     @required int runs,
@@ -146,20 +154,21 @@ class _BuiltListAddBenchmark2 extends ListBenchmarkBase {
     @required ScoreEmitter emitter,
   }) : super('BuiltList2 $size', runs: runs ~/ innerRuns, size: size, emitter: emitter);
 
-  BuiltList<int> _builtList;
+  BuiltList<int> builtList;
+  BuiltList<int> result;
 
   @override
   void setup() {
     var list = <int>[];
     for (int i = 0; i < size; i++) list.add(i);
-    _builtList = BuiltList<int>(list);
+    builtList = BuiltList<int>(list);
   }
 
   @override
   void run() {
-    ListBuilder<int> listBuilder = _builtList.toBuilder();
+    ListBuilder<int> listBuilder = builtList.toBuilder();
     for (int i = 0; i < innerRuns; i++) listBuilder.add(123);
-    listBuilder.build();
+    result = listBuilder.build();
   }
 }
 
