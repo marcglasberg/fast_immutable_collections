9286e86e90d908d4a6619ceea00b9accfa407d27
Marcelo Glasberg <marcglasberg@gmail.com>
2020-09-20 12:20:19 -0300

    Fix.

--- benchmark/cases/benchmark_add.dart
+++ benchmark/cases/benchmark_add.dart
@@ -1,12 +1,11 @@
 import 'package:benchmark_harness/benchmark_harness.dart' show ScoreEmitter;
 import 'package:built_collection/built_collection.dart' show BuiltList, ListBuilder;
+import 'package:fast_immutable_collections_benchmarks/fast_immutable_collections_benchmarks.dart';
 import 'package:kt_dart/kt.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:meta/meta.dart';
 import 'package:test/test.dart';
-
 import '../utils/list_benchmark_base.dart' show ListBenchmarkBase;
-import '../utils/table_score_emitter.dart' show TableScoreEmitter;
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////
 
--- benchmark/cases/benchmark_add_all.dart
+++ benchmark/cases/benchmark_add_all.dart
@@ -1,20 +1,17 @@
 import 'package:benchmark_harness/benchmark_harness.dart' show ScoreEmitter;
-import 'package:built_collection/built_collection.dart'
-    show BuiltList, ListBuilder;
-import 'package:fast_immutable_collections/fast_immutable_collections.dart'
-    show IList;
+import 'package:built_collection/built_collection.dart' show BuiltList, ListBuilder;
+import 'package:fast_immutable_collections/fast_immutable_collections.dart' show IList;
+import 'package:fast_immutable_collections_benchmarks/fast_immutable_collections_benchmarks.dart';
 import 'package:kt_dart/collection.dart' show KtList;
 
 import '../utils/list_benchmark_base.dart' show ListBenchmarkBase;
-import '../utils/table_score_emitter.dart' show TableScoreEmitter;
 
 final List<int> _baseList = [1, 2, 3];
 final List<int> _listToAdd = [4, 5, 6];
 
 class AddAllBenchmark {
   static void report() {
-    final TableScoreEmitter tableScoreEmitter =
-        TableScoreEmitter(reportName: 'list_add_all');
+    final TableScoreEmitter tableScoreEmitter = TableScoreEmitter(reportName: 'list_add_all');
 
     _ListAddAllBenchmark(emitter: tableScoreEmitter).report();
     _IListAddAllBenchmark(emitter: tableScoreEmitter).report();
@@ -26,8 +23,7 @@ class AddAllBenchmark {
 }
 
 class _ListAddAllBenchmark extends ListBenchmarkBase {
-  _ListAddAllBenchmark({ScoreEmitter emitter})
-      : super('List (Mutable)', emitter: emitter);
+  _ListAddAllBenchmark({ScoreEmitter emitter}) : super('List (Mutable)', emitter: emitter);
 
   List<int> _list;
 
@@ -39,8 +35,7 @@ class _ListAddAllBenchmark extends ListBenchmarkBase {
 }
 
 class _IListAddAllBenchmark extends ListBenchmarkBase {
-  _IListAddAllBenchmark({ScoreEmitter emitter})
-      : super('IList', emitter: emitter);
+  _IListAddAllBenchmark({ScoreEmitter emitter}) : super('IList', emitter: emitter);
 
   IList<int> _iList;
 
@@ -52,8 +47,7 @@ class _IListAddAllBenchmark extends ListBenchmarkBase {
 }
 
 class _KtListAddAllBenchmark extends ListBenchmarkBase {
-  _KtListAddAllBenchmark({ScoreEmitter emitter})
-      : super('KtList', emitter: emitter);
+  _KtListAddAllBenchmark({ScoreEmitter emitter}) : super('KtList', emitter: emitter);
 
   KtList<int> _ktList;
 
@@ -61,13 +55,11 @@ class _KtListAddAllBenchmark extends ListBenchmarkBase {
   void setup() => _ktList = KtList<int>.from(_baseList);
 
   @override
-  void run() =>
-      _ktList = KtList.from(List.of(_ktList.asList())..addAll(_listToAdd));
+  void run() => _ktList = KtList.from(List.of(_ktList.asList())..addAll(_listToAdd));
 }
 
 class _BuiltListAddAllBenchmark extends ListBenchmarkBase {
-  _BuiltListAddAllBenchmark({ScoreEmitter emitter})
-      : super('BuiltList', emitter: emitter);
+  _BuiltListAddAllBenchmark({ScoreEmitter emitter}) : super('BuiltList', emitter: emitter);
 
   BuiltList<int> _builtList;
 
@@ -75,6 +67,6 @@ class _BuiltListAddAllBenchmark extends ListBenchmarkBase {
   void setup() => _builtList = BuiltList<int>.of(_baseList);
 
   @override
-  void run() => _builtList.rebuild(
-      (ListBuilder<int> listBuilder) => listBuilder.addAll(_listToAdd));
+  void run() =>
+      _builtList.rebuild((ListBuilder<int> listBuilder) => listBuilder.addAll(_listToAdd));
 }
