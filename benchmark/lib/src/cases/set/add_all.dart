41d23890b8ae3d600f44dbe4c8ae656c3df383f0
Philippe Fanaro <philippefanaro@gmail.com>
2020-09-25 11:52:54 -0300

    half-way through the add_all benchmark

--- benchmark/lib/src/cases/set/add_all.dart
+++ benchmark/lib/src/cases/set/add_all.dart
@@ -9,6 +9,21 @@ import '../../utils/config.dart';
 import '../../utils/collection_benchmark_base.dart';
 import '../../utils/multi_benchmark_reporter.dart';
 
+class SetAddAllBenchmark extends MultiBenchmarkReporter<SetBenchmarkBase> {
+  static const Set<int> baseSet = {1, 2, 3}, setToAdd = {1, 2, 3, 4, 5, 6};
+
+  @override
+  final String prefixName;
+  @override
+  final List<Config> configs;
+  @override
+  final List<SetBenchmarkBase> baseBenchmarks = [
+    MutableSetAddAllBenchmark(config: null, emitter: null),
+  ];
+
+  SetAddAllBenchmark({this.prefixName = 'set_add_all', @required this.configs});
+}
+
 class MutableSetAddAllBenchmark extends SetBenchmarkBase {
   MutableSetAddAllBenchmark(
       {@required Config config, @required ScoreEmitter emitter})
@@ -21,13 +36,43 @@ class MutableSetAddAllBenchmark extends SetBenchmarkBase {
           config: newConfig ?? config, emitter: newEmitter ?? emitter);
 
   Set<int> _set;
+  Set<int> _fixedSet;
 
   @override
   Set<int> toMutable() => _set;
 
   @override
-  void setup() {}
+  void setup() => _fixedSet = Set<int>.of(SetAddAllBenchmark.baseSet);
 
   @override
-  void run() {}
+  void run() {
+    _set = Set<int>.of(_fixedSet);
+    _set.addAll(SetAddAllBenchmark.setToAdd);
+  }
+}
+
+class ISetAddAllBenchmark extends SetBenchmarkBase {
+  ISetAddAllBenchmark({@required Config config, @required ScoreEmitter emitter})
+      : super(name: 'ISet', config: config, emitter: emitter);
+
+  @override
+  ISetAddAllBenchmark reconfigure(
+          {Config newConfig, ScoreEmitter newEmitter}) =>
+      ISetAddAllBenchmark(
+          config: newConfig ?? config, emitter: newEmitter ?? emitter);
+
+  ISet<int> _iSet;
+  ISet<int> _fixedISet;
+
+  @override
+  Set<int> toMutable() => _iSet.unlock;
+
+  @override
+  void setup() => _fixedISet = ISet(SetAddAllBenchmark.baseSet);
+
+  @override
+  void run() {
+    _iSet = ISet(_fixedISet);
+    _iSet = _iSet.addAll(SetAddAllBenchmark.setToAdd);
+  }
 }
--- benchmark/test/set/add_all_test.dart
+++ benchmark/test/set/add_all_test.dart
@@ -1,10 +1,38 @@
+import 'package:fast_immutable_collections_benchmarks/src/cases/set/add_all.dart';
 import 'package:test/test.dart';
+
+import 'package:fast_immutable_collections_benchmarks/'
+    'fast_immutable_collections_benchmarks.dart';
 
 void main() {
   const int size = 10;
+  const Config config = Config(runs: 1, size: 10);
+  final Set<int> expectedSet = Set<int>.of(SetAddAllBenchmark.baseSet)
+    ..addAll(SetAddAllBenchmark.setToAdd);
 
   group('Separate Benchmarks |', () {
+    final TableScoreEmitter tableScoreEmitter =
+        TableScoreEmitter(reportName: 'set_add_all');
+
     test('`Set` (Mutable)', () {
+      final MutableSetAddAllBenchmark mutableSetAddAllBenchmark =
+          MutableSetAddAllBenchmark(config: config, emitter: tableScoreEmitter);
+
+      mutableSetAddAllBenchmark.report();
+
+      expect(mutableSetAddAllBenchmark.toMutable(), expectedSet);
+    });
+
+    test('`ISet`', () {
+      final ISetAddAllBenchmark iSetAddAllBenchmark =
+          ISetAddAllBenchmark(config: config, emitter: tableScoreEmitter);
+
+      iSetAddAllBenchmark.report();
+
+      expect(iSetAddAllBenchmark.toMutable(), expectedSet);
+    });
+
+    test('`KtSet`', () {
       
     });
   });
