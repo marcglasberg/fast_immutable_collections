0c2441f66d0bfde52a6c40d790da1ae6d56e2ab6
Philippe Fanaro <philippefanaro@gmail.com>
2020-09-25 16:05:55 -0300

    benchmark complete run is working | comment on how long the complete benchmark run takes

--- benchmark/lib/src/benchmarks.dart
+++ benchmark/lib/src/benchmarks.dart
@@ -2,6 +2,7 @@ import 'utils/collection_full_reporter.dart';
 
 /// Run the benchmarks with, for example &mdash; from the top of the project
 /// &mdash;: `dart benchmark/lib/src/benchmarks.dart`
+/// The complete benchmark run should take around 7-10 min on a good computer.
 void main() {
   ListFullReporter()..report()..save();
   SetFullReporter()..report()..save();
