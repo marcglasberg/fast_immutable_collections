9cd21e800ef5b6cb70ee69f47c70dde37518e932
Philippe Fanaro <philippefanaro@gmail.com>
2020-10-05 15:09:37 -0300

    added tests for SAdd to ensure immutability

--- test/iset/s_add_test.dart
+++ test/iset/s_add_test.dart
@@ -1,5 +1,6 @@
 import 'package:test/test.dart';
 
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:fast_immutable_collections/src/iset/s_add.dart';
 import 'package:fast_immutable_collections/src/iset/s_flat.dart';
 
@@ -10,7 +11,7 @@ void main() {
 
   test("SAdd.unlock method", () {
     expect(sAdd.unlock, <int>[1, 2, 3, 4]);
-    expect(sAdd.unlock, isA<List<int>>());
+    expect(sAdd.unlock, isA<Set<int>>());
   });
 
   test("Emptiness Properties", () {
@@ -44,8 +45,130 @@ void main() {
   });
 
   group("Ensuring Immutability |", () {
-    test("", () {
-            
+    group('SAdd.add method |', () {
+      test("Changing the passed mutable set doesn't change the LAdd", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        original.add(2);
+        original.add(3);
+
+        expect(original, <int>{1, 2, 3});
+        expect(sAdd, <int>{1, 2, 3});
+      });
+
+      test("Adding to the original SAdd doesn't change it", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        final S<int> s = sAdd.add(4);
+
+        expect(original, <int>{1, 2});
+        expect(sAdd, <int>{1, 2, 3});
+        expect(s, <int>{1, 2, 3, 4});
+      });
+
+      test("If the item being passed is a variable, a pointer to it shouldn't exist inside SAdd",
+          () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        int willChange = 4;
+        final S<int> s = sAdd.add(willChange);
+
+        willChange = 5;
+
+        expect(original, <int>{1, 2});
+        expect(sAdd, <int>{1, 2, 3});
+        expect(willChange, 5);
+        expect(s, <int>{1, 2, 3, 4});
+      });
+    });
+
+    group("SAdd.addAll method |", () {
+      test("Changing the passed mutable set doesn't change the SAdd", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        original.addAll(<int>{3, 4});
+
+        expect(original, <int>{1, 2, 3, 4});
+        expect(sAdd, <int>{1, 2, 3});
+      });
+
+      test("Changing the passed mutable list doesn't change the SAdd", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        final S<int> s = sAdd.addAll({3, 4, 5});
+
+        expect(original, <int>{1, 2});
+        expect(sAdd, <int>{1, 2, 3});
+        expect(s, <int>{1, 2, 3, 4, 5});
+      });
+
+      test(
+          "If the items being passed are from a variable, "
+          "it shouldn't have a pointer to the variable", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd1 = SAdd(sFlat, 3), sAdd2 = SAdd(sFlat, 4);
+
+        expect(sAdd1, <int>{1, 2, 3});
+        expect(sAdd2, <int>{1, 2, 4});
+
+        final S<int> s = sAdd1.addAll(sAdd2);
+        original.add(5);
+
+        expect(original, <int>{1, 2, 5});
+        expect(sAdd1, <int>{1, 2, 3});
+        expect(sAdd2, <int>{1, 2, 4});
+        expect(s, <int>{1, 2, 3, 4});
+      });
+    });
+
+    group("SAdd.remove method |", () {
+      test("Changing the passed mutable set doesn't change the SAdd", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        original.remove(2);
+
+        expect(original, <int>{1});
+        expect(sAdd, <int>{1, 2, 3});
+      });
+
+      test("Removing from the original SAdd doesn't change it", () {
+        final Set<int> original = {1, 2};
+        final SFlat<int> sFlat = SFlat(original);
+        final SAdd<int> sAdd = SAdd(sFlat, 3);
+
+        expect(sAdd, <int>{1, 2, 3});
+
+        final S<int> s = sAdd.remove(1);
+
+        expect(original, <int>{1, 2});
+        expect(sAdd, <int>{1, 2, 3});
+        expect(s, <int>{2, 3});
+      });
     });
   });
 }
