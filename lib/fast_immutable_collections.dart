21f9c1c3d27a6f4a680b0dcfd89293df94508bdf
Philippe Fanaro <philippefanaro@gmail.com>
2020-10-07 11:30:45 -0300

    put the Entry class in a separate file

--- lib/fast_immutable_collections.dart
+++ lib/fast_immutable_collections.dart
@@ -6,5 +6,6 @@ export 'src/immutable_collection.dart';
 
 export 'src/ilist/ilist.dart';
 export 'src/imap/imap.dart';
+export 'src/imap/entry.dart';
 export 'src/imap_of_sets/imap_of_sets.dart';
 export 'src/iset/iset.dart';
