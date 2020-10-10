a07589bb9df2c45a7a854df3e77e2b0533b5d3ea
Marcelo Glasberg <marcglasberg@gmail.com>
2020-10-10 11:44:30 -0300

    Implementation of ModifiableListView and ModifiableListView.

--- lib/src/ilist/modifiable_list_view.dart
+++ lib/src/ilist/modifiable_list_view.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
 import 'ilist.dart';
@@ -14,106 +16,30 @@ import 'ilist.dart';
 /// If you never mutated the list, it will be very fast to lock this list
 /// back into an [IList].
 ///
-class ModifiableListView<T> implements List<T>, CanBeEmpty {
+class ModifiableListView<T> with ListMixin<T> implements List<T>, CanBeEmpty {
   final IList<T> iList;
   List<T> list;
 
   ModifiableListView(this.iList);
 
   @override
-  bool any(bool Function(T) test) => iList.any(test);
+  T operator [](int index) => (list != null) ? list[index] : iList[index];
 
   @override
-  List<R> cast<R>() => throw UnsupportedError('cast');
+  void operator []=(int index, T value) {
+    list ??= iList.unlock;
+    list[index] == value;
+  }
 
   @override
-  bool contains(Object element) => iList.contains(element);
+  int get length => (list != null) ? list.length : iList.length;
 
   @override
-  T operator [](int index) => iList[index];
+  set length(int newLength) {
+    list ??= iList.unlock;
+    list.length == newLength;
+  }
 
-  @override
-  T elementAt(int index) => iList[index];
-
-  @override
-  bool every(bool Function(T) test) => iList.every(test);
-
-  @override
-  Iterable<E> expand<E>(Iterable<E> Function(T) f) => iList.expand(f);
-
-  @override
-  int get length => iList.length;
-
-  @override
-  T get first => iList.first;
-
-  @override
-  T get last => iList.last;
-
-  @override
-  T get single => iList.single;
-
-  @override
-  T firstWhere(bool Function(T) test, {T Function() orElse}) =>
-      iList.firstWhere(test, orElse: orElse);
-
-  @override
-  E fold<E>(E initialValue, E Function(E previousValue, T element) combine) =>
-      iList.fold(initialValue, combine);
-
-  @override
-  Iterable<T> followedBy(Iterable<T> other) => iList.followedBy(other);
-
-  @override
-  void forEach(void Function(T element) f) => iList.forEach(f);
-
-  @override
-  String join([String separator = '']) => iList.join(separator);
-
-  @override
-  T lastWhere(bool Function(T element) test, {T Function() orElse}) =>
-      iList.lastWhere(test, orElse: orElse);
-
-  @override
-  Iterable<E> map<E>(E Function(T e) f) => iList.map(f);
-
-  @override
-  T reduce(T Function(T value, T element) combine) => iList.reduce(combine);
-
-  @override
-  T singleWhere(bool Function(T element) test, {T Function() orElse}) =>
-      iList.singleWhere(test, orElse: orElse);
-
-  @override
-  Iterable<T> skip(int count) => iList.skip(count);
-
-  @override
-  Iterable<T> skipWhile(bool Function(T value) test) => iList.skipWhile(test);
-
-  @override
-  Iterable<T> take(int count) => iList.take(count);
-
-  @override
-  Iterable<T> takeWhile(bool Function(T value) test) => iList.takeWhile(test);
-
-  @override
-  Iterable<T> where(bool Function(T element) test) => iList.where(test);
-
-  @override
-  Iterable<E> whereType<E>() => iList.whereType<E>();
-
-  @override
-  bool get isEmpty => iList.isEmpty;
-
-  @override
-  bool get isNotEmpty => iList.isNotEmpty;
-
-  @override
-  Iterator<T> get iterator => iList.iterator;
-
-  @override
-  List<T> toList({bool growable = true}) => List.of(this, growable: growable);
-
-  @override
-  Set<T> toSet() => Set.of(this);
+  /// Locks the list, returning an *immutable* list ([IList]).
+  IList<T> get lock => (list != null) ? IList<T>(list) : IList<T>(iList);
 }
