9ddc01f5dc3073b093ee599080836eb4831ffb19
Philippe Fanaro <philippefanaro@gmail.com>
2020-10-15 16:53:43 -0300

    created an UnmodifiableSetView (just a template at this point)

---
+++ lib/src/iset/unmodifiable_set_view.dart
@@ -0,0 +1,41 @@
+import 'dart:collection';
+
+import 'package:meta/meta.dart';
+
+import '../immutable_collection.dart';
+import '../iset/iset.dart';
+
+@immutable
+class UnmodifiableSetView<T> with SetMixin<T> implements Set<T>, CanBeEmpty {
+  final ISet<T> _iSet;
+  final Set<T> _set;
+
+  UnmodifiableSetView(ISet<T> iSet)
+      : _iSet = iSet ?? ISet.empty<T>(),
+        _set = null;
+
+  UnmodifiableSetView.from(Set<T> set)
+      : _iSet = null,
+        _set = set;
+
+  @override
+  bool add(T value) => throw UnimplementedError('Not implemented yet.');
+
+  @override
+  bool contains(Object element) => throw UnimplementedError('Not implemented yet.');
+
+  @override
+  T lookup(Object element) => throw UnimplementedError('Not implemented yet.');
+
+  @override
+  bool remove(Object value) => throw UnimplementedError('Not implemented yet.');
+
+  @override
+  Iterator<T> get iterator => throw UnimplementedError('Not implemented yet.');
+
+  @override
+  Set<T> toSet() => throw UnimplementedError('Not implemented yet.');
+
+  @override
+  int get length => throw UnimplementedError('Not implemented yet.');
+}
---
+++ test/iset/unmodifiable_set_view_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+
+void main() {
+  group("Basic Operations |", () {
+    test("", () {
+      
+    });
+  });
+}
