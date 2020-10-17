b78484db3180b147aedd263baef0c9ca981b8c24
Philippe Fanaro <philippefanaro@gmail.com>
2020-10-16 22:52:37 -0300

    completed the ModifiableMapView

--- lib/src/imap/modifiable_map_view.dart
+++ lib/src/imap/modifiable_map_view.dart
@@ -12,19 +12,45 @@ class ModifiableMapView<K, V> with MapMixin<K, V> implements Map<K, V>, CanBeEmp
         _map = iMap == null ? {} : null;
 
   @override
-  void operator []=(K key, V value) => throw UnimplementedError('Not implemented yet.');
+  void operator []=(K key, V value) {
+    _switchToMutableMapIfNecessary();
+    _map[key] = value;
+  }
+
+  void _switchToMutableMapIfNecessary() {
+    if (_map == null) {
+      _map = _iMap.unlock;
+      _iMap = null;
+    }
+  }
 
   @override
   V operator [](Object key) => _iMap != null ? _iMap[key as K] : _map[key];
 
   @override
-  void clear() => throw UnimplementedError('Not implemented yet.');
+  void clear() {
+    _iMap = IMap.empty<K, V>();
+    _map = null;
+  }
 
   @override
-  V remove(Object key) => throw UnimplementedError('Not implemented yet.');
+  V remove(Object key) {
+    _switchToMutableMapIfNecessary();
+    return _mapRemove(key);
+  }
+
+  V _mapRemove(Object key) {
+    if (_map.containsKey(key)) {
+      final V value = _map[key];
+      _map.remove(key);
+      return value;
+    } else {
+      return null;
+    }
+  }
 
   @override
-  Iterable<K> get keys => throw UnimplementedError('Not implemented yet.');
+  Iterable<K> get keys => _iMap?.keys ?? _map.keys;
 
-  IMap<K, V> get lock => throw UnimplementedError('Not implemented yet.');
+  IMap<K, V> get lock => _iMap ?? _map.lock;
 }
--- test/imap/modifiable_map_view_test.dart
+++ test/imap/modifiable_map_view_test.dart
@@ -5,20 +5,59 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 void main() {
   const Map<String, int> baseMap = {'a': 1, 'b': 2, 'c': 3};
   final IMap<String, int> iMap = baseMap.lock;
-  final ModifiableMapView<String, int> modifiableMapView = ModifiableMapView(iMap);
 
   group("Non-mutable methods |", () {
+    final ModifiableMapView<String, int> modifiableMapView = ModifiableMapView(iMap);
+
     test("ModifiableMapView.[] operator", () {
       expect(modifiableMapView['a'], 1);
       expect(modifiableMapView['b'], 2);
       expect(modifiableMapView['c'], 3);
       expect(modifiableMapView['d'], isNull);
     });
+
+    test(
+        "ModifiableMapView.lock getter",
+        () =>
+            modifiableMapView.lock.forEach((String key, int value) => expect(baseMap[key], value)));
+
+    test(
+        "ModifiableMapView.keys",
+        () => modifiableMapView.keys
+            .forEach((String key) => expect(baseMap.containsKey(key), isTrue)));
   });
 
-  group("Mutations are allowed", () {
+  group("Mutations are allowed |", () {
+    ModifiableMapView<String, int> modifiableMapView;
+
+    setUp(() => modifiableMapView = ModifiableMapView(iMap));
+
     test("ModifiableMapView.[]= operator", () {
-      
+      expect(modifiableMapView['a'], 1);
+      modifiableMapView['a'] = 2;
+      expect(modifiableMapView['a'], 2);
+
+      expect(modifiableMapView['d'], isNull);
+      modifiableMapView['d'] = 10;
+      expect(modifiableMapView['d'], 10);
+    });
+
+    test("ModifiableMapView.clear method", () {
+      modifiableMapView.clear();
+      expect(modifiableMapView['a'], isNull);
+      expect(modifiableMapView['b'], isNull);
+      expect(modifiableMapView['c'], isNull);
+      expect(modifiableMapView.lock, <String, int>{}.lock);
+    });
+
+    test("ModifiableMapView.remove", () {
+      expect(modifiableMapView['a'], 1);
+      modifiableMapView.remove('a');
+      expect(modifiableMapView['a'], isNull);
+
+      expect(modifiableMapView['d'], isNull);
+      modifiableMapView.remove('d');
+      expect(modifiableMapView['d'], isNull);
     });
   });
 }
