201ff8a5475678af15e24430f5289d4801edbd49
Philippe Fanaro <philippefanaro@gmail.com>
2020-10-18 11:46:26 -0300

    separated ConfigList et al. into different files, otherwise it wouldn't be possible to test, due to the _isConfigLocked global variable and the lockConfig function

--- test/immutable_collection/config_list_test.dart
+++ test/immutable_collection/config_list_test.dart
@@ -3,41 +3,6 @@ import 'package:test/test.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 
 void main() {
-  group("sameCollection function |", () {
-    // TODO: Marcelo, apesar de que a documentação esclarece isso, eu tenho certeza que quem ler
-    // esse nome (`sameCollection`) vai certamente pensar que ela checa se ambos os objetos são
-    // `IList`, `ISet`, etc. Ainda acho que algo como `sameCollectionInternals` seria mais claro.
-    test("If both are null, then true", () => expect(sameCollection(null, null), isTrue));
-
-    test("If one of them is not null, then false", () {
-      expect(sameCollection(IList(), null), isFalse);
-      expect(sameCollection(null, IList()), isFalse);
-    });
-
-    test("If none of them is null, then use .same()", () {
-      final IList<int> iList1 = IList([1, 2]), iList2 = IList([1, 2]);
-      final IList<int> iList3 = iList1.remove(3);
-
-      expect(sameCollection(iList1, iList2), isFalse);
-      expect(sameCollection(iList1, iList3), isTrue);
-    });
-  });
-
-  group("disallowUnsafeConstructors |", () {
-    test("Is initially false", () => expect(disallowUnsafeConstructors, isFalse));
-
-    test("Changing the default to true", () {
-      expect(disallowUnsafeConstructors, isFalse);
-      disallowUnsafeConstructors = true;
-      expect(disallowUnsafeConstructors, isTrue);
-    });
-
-    test("Changing the lockConfig makes disallowUnsafeConstructors throw an exception", () {
-      lockConfig();
-      expect(() => disallowUnsafeConstructors = true, throwsStateError);
-    });
-  });
-
   group("ConfigList |", () {
     const ConfigList configList1 = ConfigList(), configList2 = ConfigList(isDeepEquals: false);
 
@@ -82,11 +47,22 @@ void main() {
       expect(configList1.toString(), 'ConfigList{isDeepEquals: true}');
       expect(configList2.toString(), 'ConfigList{isDeepEquals: false}');
     });
+
+    group("defaultConfigList |", () {
+      test("Is initially a ConfigList with isDeepEquals = true", () {
+        expect(defaultConfigList, const ConfigList());
+        expect(defaultConfigList.isDeepEquals, isTrue);
+      });
+
+      test("Can modify the default", () {
+        defaultConfigList = ConfigList(isDeepEquals: false);
+        expect(defaultConfigList, const ConfigList(isDeepEquals: false));
+      });
+
+      test("Changing the default ConfigList will throw an exception if lockConfig", () {
+        lockConfig();
+        expect(() => defaultConfigList = ConfigList(isDeepEquals: false), throwsStateError);
+      });
+    });
   });
-
-  // TODO: completar
-  group("ConfigSet", () {});
-
-  // TODO: completar
-  group("ConfigMap", () {});
 }
---
+++ test/immutable_collection/config_map_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
+
+void main() {
+  // TODO: completar
+  group("ConfigMap", () {});
+}
