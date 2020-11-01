27562df8710aa6ea1281f4a683cfea3b9afd8639
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-01 09:58:40 -0300

    integrated the table object with the TableScoreEmitter

--- benchmark/test/utils/table_score_emitter_test.dart
+++ benchmark/test/utils/table_score_emitter_test.dart
@@ -12,33 +12,23 @@ void main() {
     tableScoreEmitter.emit("List (Mutable)", 10);
     tableScoreEmitter.emit("Collection1", 1);
 
-    test("TableScoreEmitter.emit adds values to the TableScoreEmitter.scores",
-        () => expect(tableScoreEmitter.scores["Collection1"], 1));
-
-    test("Normalized column", () {
-      expect(tableScoreEmitter.table["normalized"]["Collection1"], 0.1);
-      expect(tableScoreEmitter.table["normalized"]["List (Mutable)"], 1);
-    });
-
-    test("Normalized column against the mutable list", () {
-      tableScoreEmitter.emit("Collection2", 100);
-
-      expect(tableScoreEmitter.table["normalizedAgainstList"]["Collection2"], 10);
-    });
-
-    test("Normalized column against runs (time / runs)", () {
-      expect(tableScoreEmitter.table["normalizedAgainstRuns"]["List (Mutable)"], .1);
-      expect(tableScoreEmitter.table["normalizedAgainstRuns"]["Collection1"], .01);
-      expect(tableScoreEmitter.table["normalizedAgainstRuns"]["Collection2"], 1);
+    test("TableScoreEmitter.emit adds values to the TableScoreEmitter.table", () {
+      expect(tableScoreEmitter.table.resultsColumn.records, [
+        StopwatchRecord(collectionName: "List (Mutable)", record: 10),
+        StopwatchRecord(collectionName: "Collection1", record: 1),
+      ]);
     });
   });
 
   group("Other stuff |", () {
-    final TableScoreEmitter tableScoreEmitter = TableScoreEmitter(prefixName: "report");
+    final TableScoreEmitter tableScoreEmitter =
+        TableScoreEmitter(prefixName: "report", config: Config(runs: 10, size: 10));
 
-    tableScoreEmitter.emit("Test1", 1);
+    tableScoreEmitter.emit("Test1", 5);
 
-    test("TableScoreEmitter.toString method",
-        () => expect(tableScoreEmitter.toString(), "Table Score Emitter: {Test1: 1.0}"));
+    test(
+        "TableScoreEmitter.toString method",
+        () => expect(tableScoreEmitter.toString(),
+            "Table Score Emitter: RecordsColumn: [StopwatchRecord: (collectionName: Test1, record: 5.0)]"));
   });
 }
