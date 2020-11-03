cfb87114459ce7aaf8d7744e5535a59f91579ec4
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-03 19:32:32 -0300

    first (ugly) sketch of the landing page for the example app

--- benchmark/example/lib/main.dart
+++ benchmark/example/lib/main.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 
 import "package:fast_immutable_collections_benchmarks/fast_immutable_collections_benchmarks.dart";
 
-import "screens/multi_benchmark_screen.dart";
+import "widgets/button.dart";
 
 void main() => runApp(BenchmarkApp());
 
@@ -29,8 +29,68 @@ class BenchmarkApp extends StatelessWidget {
             style: TextStyle(color: Colors.white, height: 1.3),
           ),
         ),
-        body: MultiBenchmarkScreen(),
+        body: CollectionChoiceScreen(),
       ),
     );
+  }
+}
+
+class CollectionChoiceScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Color(0xFFCCCCCC),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 40),
+            Text(
+              "Choose a collection type to benchmark:",
+              style: const TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 40),
+            Expanded(
+                child: SingleChildScrollView(
+                    child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Card(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            button(
+                              label: "List",
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            button(
+                              label: "Map",
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            button(
+                              label: "Set",
+                              onPressed: () {},
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )))
+          ],
+        ));
   }
 }
