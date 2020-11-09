dd62ed5b73a7174676a891e3f9ac43360df47671
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-09 14:46:12 -0300

    added a rough sketch of the bottom navigation bar | added the missing material design kit to the app's pubspec

--- benchmark/example/lib/screens/graph_screen.dart
+++ benchmark/example/lib/screens/graph_screen.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
 import "package:fast_immutable_collections_benchmarks/fast_immutable_collections_benchmarks.dart";
 
 import "../widgets/bar_chart.dart";
@@ -10,6 +11,14 @@ class GraphScreen extends StatelessWidget {
 
   const GraphScreen({@required this.title, @required this.recordsTable});
 
+  static final IList<Icon> _sizeIcons = const [
+    Icon(Icons.filter_1),
+    Icon(Icons.filter_2),
+    Icon(Icons.filter_3),
+    Icon(Icons.filter_4),
+    Icon(Icons.filter_5),
+  ].lock;
+
   @override
   Widget build(_) {
     return Scaffold(
@@ -19,6 +28,22 @@ class GraphScreen extends StatelessWidget {
       body: Container(
         padding: const EdgeInsets.only(left: 5),
         child: BarChart(recordsTable: recordsTable),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: _sizeIcons[0],
+            label: "Size: 1",
+          ),
+          BottomNavigationBarItem(
+            icon: _sizeIcons[1],
+            label: "Size: 2",
+          ),
+          BottomNavigationBarItem(
+            icon: _sizeIcons[2],
+            label: "Size: 3",
+          ),
+        ],
       ),
     );
   }
