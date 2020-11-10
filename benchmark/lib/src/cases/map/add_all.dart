3dd995cde9bc3ea10074e5d222621a868d95b8f9
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-10 14:14:18 -0300

    finished MapAddAll

--- benchmark/lib/src/cases/map/add_all.dart
+++ benchmark/lib/src/cases/map/add_all.dart
@@ -16,6 +16,9 @@ class MapAddAllBenchmark extends MultiBenchmarkReporter<MapBenchmarkBase> {
   MapAddAllBenchmark({@required TableScoreEmitter emitter})
       : benchmarks = <MapBenchmarkBase>[
           MutableMapAddAllBenchmark(emitter: emitter),
+          IMapAddAllBenchmark(emitter: emitter),
+          KtMapAddAllBenchmark(emitter: emitter),
+          BuiltMapAddAllBenchmark(emitter: emitter),
         ].lock,
         super(emitter: emitter);
 }
@@ -38,4 +41,56 @@ class MutableMapAddAllBenchmark extends MapBenchmarkBase {
     _map = Map<String, int>.of(_fixedMap);
     _map.addAll(MapAddAllBenchmark.mapToAdd);
   }
+}
+
+class IMapAddAllBenchmark extends MapBenchmarkBase {
+  IMapAddAllBenchmark({@required TableScoreEmitter emitter})
+      : super(name: "IMap", emitter: emitter);
+
+  IMap<String, int> _iMap;
+  IMap<String, int> _result;
+
+  @override
+  Map<String, int> toMutable() => _result.unlock;
+
+  @override
+  void setup() => _iMap = IMap<String, int>(MapAddAllBenchmark.baseMap);
+
+  @override
+  void run() => _result = _iMap.addAll(MapAddAllBenchmark.mapToAdd.lock);
+}
+
+class KtMapAddAllBenchmark extends MapBenchmarkBase {
+  KtMapAddAllBenchmark({@required TableScoreEmitter emitter})
+      : super(name: "KtMap", emitter: emitter);
+
+  KtMap<String, int> _ktMap;
+  KtMap<String, int> _result;
+
+  @override
+  Map<String, int> toMutable() => _result.asMap();
+
+  @override
+  void setup() => _ktMap = KtMap<String, int>.from(MapAddAllBenchmark.baseMap);
+
+  @override
+  void run() => _result = _ktMap.plus(KtMap<String, int>.from(MapAddAllBenchmark.mapToAdd));
+}
+
+class BuiltMapAddAllBenchmark extends MapBenchmarkBase {
+  BuiltMapAddAllBenchmark({@required TableScoreEmitter emitter})
+      : super(name: "KtMap", emitter: emitter);
+
+  BuiltMap<String, int> _builtMap;
+  BuiltMap<String, int> _result;
+
+  @override
+  Map<String, int> toMutable() => _result.asMap();
+
+  @override
+  void setup() => _builtMap = BuiltMap<String, int>.of(MapAddAllBenchmark.baseMap);
+
+  @override
+  void run() => _result = _builtMap.rebuild(
+      (MapBuilder<String, int> mapBuilder) => mapBuilder.addAll(MapAddAllBenchmark.mapToAdd));
 }
--- benchmark/test/map/add_all_test.dart
+++ benchmark/test/map/add_all_test.dart
@@ -16,6 +16,45 @@ void main() {
         ..addAll(MapAddAllBenchmark.mapToAdd);
       expect(mapAddAllBenchmark.toMutable(), expectedMap);
     });
+
+    test("IMap", () {
+      final TableScoreEmitter tableScoreEmitter =
+          TableScoreEmitter(prefixName: "add_map_iMap", config: Config(runs: 100, size: 10));
+      final IMapAddAllBenchmark iMapAddAllBenchmark =
+          IMapAddAllBenchmark(emitter: tableScoreEmitter);
+
+      iMapAddAllBenchmark.report();
+
+      final Map<String, int> expectedMap = Map.of(MapAddAllBenchmark.baseMap)
+        ..addAll(MapAddAllBenchmark.mapToAdd);
+      expect(iMapAddAllBenchmark.toMutable(), expectedMap);
+    });
+
+    test("KtMap", () {
+      final TableScoreEmitter tableScoreEmitter =
+          TableScoreEmitter(prefixName: "add_map_ktMap", config: Config(runs: 100, size: 10));
+      final KtMapAddAllBenchmark ktMapAddAllBenchmark =
+          KtMapAddAllBenchmark(emitter: tableScoreEmitter);
+
+      ktMapAddAllBenchmark.report();
+
+      final Map<String, int> expectedMap = Map.of(MapAddAllBenchmark.baseMap)
+        ..addAll(MapAddAllBenchmark.mapToAdd);
+      expect(ktMapAddAllBenchmark.toMutable(), expectedMap);
+    });
+
+    test("BuiltMap", () {
+      final TableScoreEmitter tableScoreEmitter =
+          TableScoreEmitter(prefixName: "add_map_builtMap", config: Config(runs: 100, size: 10));
+      final BuiltMapAddAllBenchmark builtMapAddAllBenchmark =
+          BuiltMapAddAllBenchmark(emitter: tableScoreEmitter);
+
+      builtMapAddAllBenchmark.report();
+
+      final Map<String, int> expectedMap = Map.of(MapAddAllBenchmark.baseMap)
+        ..addAll(MapAddAllBenchmark.mapToAdd);
+      expect(builtMapAddAllBenchmark.toMutable(), expectedMap);
+    });
   });
 
   group("Multiple Benchmarks |", () {
