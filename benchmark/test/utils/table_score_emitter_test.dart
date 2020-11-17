fb2f37862d0fcaa195da59e0220776efcac38589
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-17 09:59:10 -0300

    refactored table score emitter tests

--- benchmark/test/utils/table_score_emitter_test.dart
+++ benchmark/test/utils/table_score_emitter_test.dart
@@ -4,15 +4,15 @@ import "package:fast_immutable_collections_benchmarks/fast_immutable_collections
 
 void main() {
   group("Table and Scores |", () {
-    const Config config = Config(runs: 100, size: 100);
-
-    final TableScoreEmitter tableScoreEmitter =
-        TableScoreEmitter(prefixName: "report", config: config);
-
-    tableScoreEmitter.emit("List (Mutable)", 10);
-    tableScoreEmitter.emit("Collection1", 1);
-
     test("TableScoreEmitter.emit adds values to the TableScoreEmitter.table", () {
+      const Config config = Config(runs: 100, size: 100);
+
+      final TableScoreEmitter tableScoreEmitter =
+          TableScoreEmitter(prefixName: "report", config: config);
+
+      tableScoreEmitter.emit("List (Mutable)", 10);
+      tableScoreEmitter.emit("Collection1", 1);
+
       expect(tableScoreEmitter.table.resultsColumn.records, [
         StopwatchRecord(collectionName: "List (Mutable)", record: 10),
         StopwatchRecord(collectionName: "Collection1", record: 1),
@@ -21,14 +21,14 @@ void main() {
   });
 
   group("Other stuff |", () {
-    final TableScoreEmitter tableScoreEmitter =
-        TableScoreEmitter(prefixName: "report", config: Config(runs: 10, size: 10));
+    test("TableScoreEmitter.toString method", () {
+      final TableScoreEmitter tableScoreEmitter =
+          TableScoreEmitter(prefixName: "report", config: Config(runs: 10, size: 10));
 
-    tableScoreEmitter.emit("Test1", 5);
+      tableScoreEmitter.emit("Test1", 5);
 
-    test(
-        "TableScoreEmitter.toString method",
-        () => expect(tableScoreEmitter.toString(),
-            "Table Score Emitter: RecordsColumn: [StopwatchRecord: (collectionName: Test1, record: 5.0)]"));
+      expect(tableScoreEmitter.toString(),
+          "Table Score Emitter: RecordsColumn: [StopwatchRecord: (collectionName: Test1, record: 5.0)]");
+    });
   });
 }
