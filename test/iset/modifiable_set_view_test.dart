fed50659c390007835576e5d488fcb193d285840
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-18 09:17:19 -0300

    refactored modifiable_set_view_test

--- test/iset/modifiable_set_view_test.dart
+++ test/iset/modifiable_set_view_test.dart
@@ -3,69 +3,78 @@ import "package:test/test.dart";
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 
 void main() {
-  const Set<int> baseSet = {1, 2, 3};
-  final ISet<int> iSet = baseSet.lock;
-
-  group("Non-mutable operations |", () {
+  test("Non-mutable operations | ModifiableSetView.length getter", () {
+    const Set<int> baseSet = {1, 2, 3};
+    final ISet<int> iSet = baseSet.lock;
     final ModifiableSetView<int> modifiableSetView = ModifiableSetView(iSet);
-
-    test("ModifiableSetView.length getter", () => expect(modifiableSetView.length, baseSet.length));
-
-    test("ModifiableSetView.lock getter",
-        () => expect(modifiableSetView.lock, allOf(isA<ISet<int>>(), baseSet)));
-
-    test("Emptiness Properties", () {
-      expect(modifiableSetView.isEmpty, isFalse);
-      expect(modifiableSetView.isNotEmpty, isTrue);
-    });
-
-    test("ModifiableSetView.contains method", () {
-      expect(modifiableSetView.contains(1), isTrue);
-      expect(modifiableSetView.contains(2), isTrue);
-      expect(modifiableSetView.contains(3), isTrue);
-      expect(modifiableSetView.contains(4), isFalse);
-    });
-
-    test("ModifiableSetView.iterator getter", () {
-      final Iterator<int> iterator = modifiableSetView.iterator;
-
-      int count = 0;
-      final Set<int> result = {};
-      while (iterator.moveNext()) {
-        count++;
-        result.add(iterator.current);
-      }
-      expect(count, baseSet.length);
-      expect(result, baseSet);
-    });
-
-    test("ModifiableSetView.toSet method", () => expect(modifiableSetView.toSet(), baseSet));
-
-    test("ModifiableSetView.lookup method", () {
-      expect(modifiableSetView.lookup(1), 1);
-      expect(modifiableSetView.lookup(2), 2);
-      expect(modifiableSetView.lookup(3), 3);
-      expect(modifiableSetView.lookup(4), null);
-    });
+    expect(modifiableSetView.length, baseSet.length);
   });
 
-  group("Mutations are allowed |", () {
-    ModifiableSetView<int> modifiableSetView;
+  test("Non-mutable operations | ModifiableSetView.lock getter", () {
+    const Set<int> baseSet = {1, 2, 3};
+    final ISet<int> iSet = baseSet.lock;
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView(iSet);
+    expect(modifiableSetView.lock, allOf(isA<ISet<int>>(), baseSet));
+  });
 
-    setUp(() => modifiableSetView = ModifiableSetView(iSet));
+  test("Non-mutable operations | isEmpty | isNotEmpty", () {
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView({1, 2, 3}.lock);
+    expect(modifiableSetView.isEmpty, isFalse);
+    expect(modifiableSetView.isNotEmpty, isTrue);
+  });
 
-    test("ModifiableSetView.add method", () {
-      expect(modifiableSetView.add(3), isFalse);
-      expect(modifiableSetView.add(4), isTrue);
-      expect(modifiableSetView.length, 4);
-      expect(modifiableSetView.contains(4), isTrue);
-    });
+  test("Non-mutable operations | ModifiableSetView.contains()", () {
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView({1, 2, 3}.lock);
+    expect(modifiableSetView.contains(1), isTrue);
+    expect(modifiableSetView.contains(2), isTrue);
+    expect(modifiableSetView.contains(3), isTrue);
+    expect(modifiableSetView.contains(4), isFalse);
+  });
 
-    test("ModifiableSetView.remove method", () {
-      expect(modifiableSetView.remove(4), isFalse);
-      expect(modifiableSetView.remove(3), isTrue);
-      expect(modifiableSetView.length, 2);
-      expect(modifiableSetView.contains(3), isFalse);
-    });
+  test("Non-mutable operations | ModifiableSetView.iterator getter", () {
+    const Set<int> baseSet = {1, 2, 3};
+    final ISet<int> iSet = baseSet.lock;
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView(iSet);
+    final Iterator<int> iterator = modifiableSetView.iterator;
+
+    int count = 0;
+    final Set<int> result = {};
+    while (iterator.moveNext()) {
+      count++;
+      result.add(iterator.current);
+    }
+    expect(count, baseSet.length);
+    expect(result, baseSet);
+  });
+
+  test("Non-mutable operations | ModifiableSetView.toSet()", () {
+    const Set<int> baseSet = {1, 2, 3};
+    final ISet<int> iSet = baseSet.lock;
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView(iSet);
+    expect(modifiableSetView.toSet(), baseSet);
+  });
+
+  test("Non-mutable operations | ModifiableSetView.lookup()", () {
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView({1, 2, 3}.lock);
+    expect(modifiableSetView.lookup(1), 1);
+    expect(modifiableSetView.lookup(2), 2);
+    expect(modifiableSetView.lookup(3), 3);
+    expect(modifiableSetView.lookup(4), null);
+  });
+
+  test("Mutations are allowed | ModifiableSetView.add method", () {
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView({1, 2, 3}.lock);
+    expect(modifiableSetView.add(3), isFalse);
+    expect(modifiableSetView.add(4), isTrue);
+    expect(modifiableSetView.length, 4);
+    expect(modifiableSetView.contains(4), isTrue);
+  });
+
+  test("Mutations are allowed | ModifiableSetView.remove method", () {
+    final ModifiableSetView<int> modifiableSetView = ModifiableSetView({1, 2, 3}.lock);
+    expect(modifiableSetView.remove(4), isFalse);
+    expect(modifiableSetView.remove(3), isTrue);
+    expect(modifiableSetView.length, 2);
+    expect(modifiableSetView.contains(3), isFalse);
   });
 }
