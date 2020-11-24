eee025d8f854cbbc260bbd94eda9d86f57d91584
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-24 10:09:53 -0300

    completed tests for UnmodifiableListView

--- test/ilist/unmodifiable_list_view_test.dart
+++ test/ilist/unmodifiable_list_view_test.dart
@@ -3,6 +3,14 @@ import "package:test/test.dart";
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
 
 void main() {
+  test("Simple Empty Initialization", () {
+    final UnmodifiableListView unmodifiableListView1 = UnmodifiableListView([].lock);
+    final UnmodifiableListView unmodifiableListView2 = UnmodifiableListView(null);
+
+    expect(unmodifiableListView1.isEmpty, isTrue);
+    expect(unmodifiableListView2.isEmpty, isTrue);
+  });
+
   test("Non-mutable operations | UnmodifiableListView.[] operator", () {
     const List<int> baseList = [1, 2, 3];
     final UnmodifiableListView<int> unmodifiableListView = UnmodifiableListView(baseList.lock),
@@ -96,6 +104,19 @@ void main() {
 
     views.forEach(
         (UnmodifiableListView<int> view) => expect(() => view.add(4), throwsUnsupportedError));
+  });
+
+  test("Mutations are not allowed | UnmodifiableListView.addAll method", () {
+    const List<int> baseList = [1, 2, 3];
+    final UnmodifiableListView<int> unmodifiableListView = UnmodifiableListView(baseList.lock),
+        unmodifiableListViewFromList = UnmodifiableListView.fromList(baseList);
+    final List<UnmodifiableListView<int>> views = [
+      unmodifiableListView,
+      unmodifiableListViewFromList,
+    ];
+
+    views.forEach((UnmodifiableListView<int> view) =>
+        expect(() => view.addAll([4, 5]), throwsUnsupportedError));
   });
 
   test("Mutations are not allowed | UnmodifiableListView.remove method", () {
