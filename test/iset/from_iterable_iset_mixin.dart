7932c7388799b882c0f05a84995308199f8f81a6
Philippe Fanaro <philippefanaro@gmail.com>
2020-11-26 13:55:29 -0300

    set up for tests for FromIterableISetMixin

---
+++ test/iset/from_iterable_iset_mixin.dart
@@ -0,0 +1,67 @@
+import "package:test/test.dart";
+
+import "package:fast_immutable_collections/fast_immutable_collections.dart";
+
+void main() {
+  test("FromIterableISetMixin.any()", () {
+    
+  });
+
+  test("FromIterableISetMixin.cast()", () {});
+
+  test("FromIterableISetMixin.contains()", () {});
+
+  test("FromIterableISetMixin.elementAt()", () {});
+
+  test("FromIterableISetMixin.every()", () {});
+
+  test("FromIterableISetMixin.expand()", () {});
+
+  test("FromIterableISetMixin.length", () {});
+
+  test("FromIterableISetMixin.first", () {});
+
+  test("FromIterableISetMixin.last", () {});
+
+  test("FromIterableISetMixin.single", () {});
+
+  test("FromIterableISetMixin.firstWhere()", () {});
+
+  test("FromIterableISetMixin.fold()", () {});
+
+  test("FromIterableISetMixin.followedBy()", () {});
+
+  test("FromIterableISetMixin.forEach()", () {});
+
+  test("FromIterableISetMixin.join()", () {});
+
+  test("FromIterableISetMixin.lastWhere()", () {});
+
+  test("FromIterableISetMixin.map()", () {});
+
+  test("FromIterableISetMixin.reduce()", () {});
+
+  test("FromIterableISetMixin.singleWhere()", () {});
+
+  test("FromIterableISetMixin.skip()", () {});
+
+  test("FromIterableISetMixin.skipWhile()", () {});
+
+  test("FromIterableISetMixin.take()", () {});
+
+  test("FromIterableISetMixin.takeWhile()", () {});
+
+  test("FromIterableISetMixin.where()", () {});
+
+  test("FromIterableISetMixin.whereType()", () {});
+
+  test("FromIterableISetMixin.isEmpty", () {});
+
+  test("FromIterableISetMixin.isNotEmpty", () {});
+
+  test("FromIterableISetMixin.iterator", () {});
+
+  test("FromIterableISetMixin.toList()", () {});
+
+  test("FromIterableISetMixin.toSet()", () {});
+}
