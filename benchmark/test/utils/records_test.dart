1f0bfbbaffedc8b2879547ec151981f2429ed28e
Philippe Fanaro <pidphil@gmail.com>
2021-02-01 20:24:29 -0300

    reorganized top pubspec to support Dart when publishing

--- benchmark/test/utils/records_test.dart
+++ benchmark/test/utils/records_test.dart
@@ -1,5 +1,4 @@
 import "package:fast_immutable_collections/fast_immutable_collections.dart";
-import "package:matcher/matcher.dart";
 import "package:test/test.dart";
 
 import "package:fast_immutable_collections_benchmarks/fast_immutable_collections_benchmarks.dart";
