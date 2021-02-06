31107f943598e728de6e877a87728fb8cf51e0b6
Philippe Fanaro <pidphil@gmail.com>
2021-02-06 14:01:53 -0300

    another test to check why the CI/CD isn't working

--- test/run_all_tests.dart
+++ test/run_all_tests.dart
@@ -4,10 +4,22 @@ import "base/config_list_test.dart" as base_config_list_test;
 // 2) ilist
 import "ilist/ilist_test.dart" as ilist_test;
 
+// 3) iset
+import "iset/iset_test.dart" as iset_test;
+
+// 4) imap
+import "imap/imap_test.dart" as imap_test;
+
 void main() {
   // 1) base
   base_config_list_test.main();
 
   // 2) ilist
   ilist_test.main();
+
+  // 3) iset
+  iset_test.main();
+
+  // 4) imap
+  imap_test.main();
 }
